80283f1a84f22a7d4bb8c2200829c536f6870a6e
Gboyega Dada <boyega@gmail.com>
2016-05-01 18:00:48 +0100

    ...

--- examples/index.php
+++ examples/index.php
@@ -19,6 +19,4 @@ $vars = [
 
 $n = new Nano\Engine();
 $n->setPath(__DIR__);
-echo $n->render('base.html.php', $vars);
-echo $n->render('hello.html.php', $vars);
 echo $n->render('hello-again.html.php', $vars);
